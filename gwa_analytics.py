3fcbc0a786b41b7f9b13189c51f530bfd8552642
jlnshly <jelaineashleyramos@gmail.com>
2026-04-25 23:46:54 +0800

    gwa finder progress

---
+++ gwa_analytics.py
@@ -0,0 +1,7 @@
+class GradeProcessor:
+    """Will do all the math or processing for finding the highest gwa"""
+    def __init__(self, student_list):
+        self.students = student_list
+
+    def find_highest_gwa(self):
+
