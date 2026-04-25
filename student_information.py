fa79add3b559cb4c4d46fe7a8da97302a1674133
jlnshly <jelaineashleyramos@gmail.com>
2026-04-25 22:35:34 +0800

    updated errors and code

--- student_information.py
+++ student_information.py
@@ -1,7 +1,11 @@
 class Student:
     """Keeping student records"""
-    def __init__(self, first_name, last_name, age, gender):
+    def __init__(self, first_name = str, last_name = str, gwa = float):
         self.first_name = first_name
         self.last_name = last_name
-        self.gwa = age
+        self.gwa = gwa
+
+    def __repr__(self):
+        return f'{self.first_name} {self.last_name} {self.gwa}'
+
 
