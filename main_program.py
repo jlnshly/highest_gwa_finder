36970454511351a26f8122849c15f82338d1c8f5
jlnshly <jelaineashleyramos@gmail.com>
2026-04-26 00:00:20 +0800

    code done

--- main_program.py
+++ main_program.py
@@ -2,4 +2,15 @@ import os
 from student_information import Student
 from gwa_analytics import GradeProcessor
 
-def execute()
+def execute():
+    with open('students.txt', 'r') as f:
+        lines = f.readlines()
+    student_objects = []
+    for line in lines:
+        first_name, last_name, gwa = line.strip().split(',')
+        student_objects.append(Student(first_name.strip(), last_name.strip(), float(gwa.strip())))
+    engine = GradeProcessor(student_objects)
+    winner = engine.find_highest_gwa()
+    print(f'{winner.first_name} {winner.last_name} with {winner.gwa}')
+
+
