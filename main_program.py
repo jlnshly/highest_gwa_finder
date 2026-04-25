8530601d617902f388622c55cbf8e37b4d949ea2
jlnshly <jelaineashleyramos@gmail.com>
2026-04-26 00:27:04 +0800

    final code done working

--- main_program.py
+++ main_program.py
@@ -3,14 +3,26 @@ from student_information import Student
 from gwa_analytics import GradeProcessor
 
 def execute():
+    if not os.path.exists('students.txt'):
+        print('No students.txt found')
     with open('students.txt', 'r') as f:
         lines = f.readlines()
     student_objects = []
     for line in lines:
-        first_name, last_name, gwa = line.strip().split(',')
-        student_objects.append(Student(first_name.strip(), last_name.strip(), float(gwa.strip())))
+        try:
+            first_name, last_name, gwa = line.strip().split(',')
+            student_objects.append(Student(first_name.strip(), last_name.strip(), float(gwa.strip())))
+        except ValueError:
+            continue
     engine = GradeProcessor(student_objects)
-    winner = engine.find_highest_gwa()
-    print(f'{winner.first_name} {winner.last_name} with {winner.gwa}')
+    highest = engine.find_highest_gwa()
+    if highest:
+        for h in highest:
+            print(f'{h.last_name}, {h.first_name} with {h.gwa}')
 
+    else:
+        print('Student information is not available')
+
+if __name__ == '__main__':
+    execute()
 
